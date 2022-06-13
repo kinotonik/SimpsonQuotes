0ad0a7aedfec855d8b7b6ec0ca469016d3ed99a3
kinotonik <zedmed@kinotonik.fr>
2022-06-13 16:17:29 +0200

    add img logo default

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 const sampleSimpson =   
   
     {
-    "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629",
+    "image": "https://www.nicepng.com/png/full/41-416689_share-this-image-homer-simpson-fat-png.png",
     }
 
 function App() {
